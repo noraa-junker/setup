329662cae545d283b3eb6ff1429b2806c19ea100
Aaron Junker <Aaron.Junker@outlook.com>
2024-04-18 01:24:11 +0200

    Add git config

--- setupDotfiles.ps1
+++ setupDotfiles.ps1
@@ -2,3 +2,6 @@ Write-Host "Setting up dotfiles..."
 
 Write-Host "Copying .vimrc to $env:USERPROFILE"
 Copy-Item -Path .vimrc -Destination "$($env:USERPROFILE)\.vimrc" -Force
+
+Write-Host "Copying .gitconfig to $env:USERPROFILE"
+Copy-Item -Path .gitconfig -Destination "$($env:USERPROFILE)\.gitconfig" -Force
