a4e639430dc3dd821c4399d6d444fe2d54eede73
Aaron Junker <Aaron.Junker@outlook.com>
2025-01-30 17:38:04 +0100

    Make dotfiles to hardlinks

--- setupDotfiles.ps1
+++ setupDotfiles.ps1
@@ -1,19 +1,30 @@
+function MakeLink{
+    param(
+        [string]$link,
+        [string]$target
+    )
+    if (Test-Path $target) {
+        Remove-Item $target -Force
+    }
+    New-Item -Path $target -ItemType HardLink -Value $link
+}
+
 Write-Host "Copying .vimrc to $env:USERPROFILE"
-Copy-Item -Path .\dotfiles\.vimrc -Destination "$($env:USERPROFILE)\.vimrc" -Force
+MakeLink -target "$($env:USERPROFILE)\.vimrc" -link ".\dotfiles\.vimrc"
 
 Write-Host "Copying .gitconfig to $env:USERPROFILE"
-Copy-Item -Path .\dotfiles\.gitconfig -Destination "$($env:USERPROFILE)\.gitconfig" -Force
+MakeLink -target "$($env:USERPROFILE)\.gitconfig" -link ".\dotfiles\.gitconfig"
 
 Write-Host "Copying Terminal Config to $env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState"
-Copy-Item -Path .\dotfiles\terminal-settings.json -Destination "$($env:USERPROFILE)\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -Force
+MakeLink -target "$($env:USERPROFILE)\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -link ".\dotfiles\terminal-settings.json"
 
 $documents = [Environment]::GetFolderPath("MyDocuments")
 
 Write-Host "PowerShell profile setup"
-Copy-Item -Path .\dotfiles\Microsoft.PowerShell_profile.ps1 -Destination "$($documents)\PowerShell\Microsoft.PowerShell_profile.ps1" -Force
+MakeLink -target "$($documents)\PowerShell\Microsoft.PowerShell_profile.ps1" -link ".\dotfiles\Microsoft.PowerShell_profile.ps1"
 
 Write-Host "Setting up winget settings"
-Copy-Item -Path .\dotfiles\winget-settings.json -Destination "$($env:USERPROFILE)\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json" -Force
+MakeLink -target "$($env:USERPROFILE)\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json" -link ".\dotfiles\winget-settings.json"
 
 Write-Host "Set up config for legacy Windows PowerShell"
-Copy-Item -Path .\dotfiles\Microsoft.WindowsPowerShell_profile.ps1 -Destination "$($env:USERPROFILE)\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Force
+MakeLink -target "$($env:USERPROFILE)\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -link ".\dotfiles\Microsoft.WindowsPowerShell_profile.ps1"
