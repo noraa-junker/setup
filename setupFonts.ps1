a2c2a949ad111b044050b0a14d76a57b04998b34
Aaron Junker <Aaron.Junker@outlook.com>
2025-05-21 11:22:58 +0200

    Fix font installing, update help for PowerShell 7 and dev dsc confirm dialog

--- setupFonts.ps1
+++ setupFonts.ps1
@@ -1,4 +1,8 @@
 $tempFolder = $env:TEMP
+if (Test-Path "$tempFolder\CascadiaCode") {
+    Remove-Item "$tempFolder\CascadiaCode" -Force -Recurse -Confirm:$false
+}
+$localAppDataFolder = $env:localAppData
 $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
 Invoke-WebRequest "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaCode.zip" -OutFile "$tempfolder\CascadiaCode.zip"
 Expand-Archive -Path "$tempfolder\CascadiaCode.zip" -DestinationPath "$tempfolder\CascadiaCode"
@@ -6,7 +10,7 @@ $location = Get-Location
 Set-Location "$tempfolder\CascadiaCode"
 foreach ($file in Get-ChildItem *.ttf) {
     $fileName = $file.Name
-    if (-not (Test-Path -Path "C:\Windows\fonts\$fileName")) {
+    if (-not (Test-Path -Path "$localAppDataFolder\Microsoft\Windows\Fonts\$fileName")) {
         Write-Output $fileName
         Get-Item $file | ForEach-Object { $fonts.CopyHere($_.FullName) }
     }
